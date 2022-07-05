52bbda9d10d9674936a863181efb1dfa563029f7
Kham Laung <shwegyimontkyut@gmail.com>
2022-07-05 10:17:08 +0630

    Added changes for deployment to Heroku

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://express-library-app:node-app@cluster0.6ug2m.mongodb.net/?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://express-library-app:node-app@cluster0.6ug2m.mongodb.net/?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
