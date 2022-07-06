e31fe4ec3619d051a754e2df8c6daf71eb178aa9
Kham Laung <shwegyimontkyut@gmail.com>
2022-07-06 11:39:37 +0630

    configure mongouri

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var compression = require('compression');
 var helmet = require('helmet');
 require('dotenv').config();
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,8 +15,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = PROCESS.env.DB_URI;
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoDB = process.env.MONGO_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
